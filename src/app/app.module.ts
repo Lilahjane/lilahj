130a74b35380752e5ed9fa3499618456744f2bc7
Lilahjane <149263011+Lilahjane@users.noreply.github.com>
2024-01-05 08:07:19 -0500

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle'; // Import MatSlideToggleModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,25 +14,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule // Add MatSlideToggleModule to imports
+    MatButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
 import {Component} from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
-
-/**
- * @title Basic buttons
- */
-@Component({
-  selector: 'button-overview-example',
-  templateUrl: 'button-overview-example.html',
-  styleUrls: ['button-overview-example.css'],
-  standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule],
-})
-export class ButtonOverviewExample {}
